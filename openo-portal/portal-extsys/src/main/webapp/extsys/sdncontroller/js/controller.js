85c2d72af3b831499e681de357abd3c12205273e
MR-ZZC <zhaozhongchao@huawei.com>
2016-09-22 19:07:11 +0800

    Complete functions of sdnocontroller.
    Change-Id: I78963ead8487672b459290cb363a767f0e6dca80
    Signed-off-by: MR-ZZC <zhaozhongchao@huawei.com>

--- openo-portal/portal-extsys/src/main/webapp/extsys/sdncontroller/js/controller.js
+++ openo-portal/portal-extsys/src/main/webapp/extsys/sdncontroller/js/controller.js
@@ -12,148 +12,160 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
-$('.siteDeleteImg').click(function(){
-	var data = $(this).parent().parent().parent().find('td:last').find('div:last').html();
-	alert(data);
-	var jsonObj = JSON.parse(data);
-	for(var i = 0; i < jsonObj.length; i++) {
-	    var obj = jsonObj[i];
-		var rowData = [obj.tpName,obj.peName,obj.vlanId,obj.siteCidr,obj.ip];
-		$('#underlayTpDataTable').DataTable();
-		$('#underlayTpDataTable').dataTable().fnAddData(rowData);
-	}
-});
 
+$('.siteDeleteImg').click(
+		function() {
+			var data = $(this).parent().parent().parent().find('td:last').find(
+					'div:last').html();
+			alert(data);
+			var jsonObj = JSON.parse(data);
+			for (var i = 0; i < jsonObj.length; i++) {
+				var obj = jsonObj[i];
+				var rowData = [ obj.tpName, obj.peName, obj.vlanId,
+						obj.siteCidr, obj.ip ];
+				$('#underlayTpDataTable').DataTable();
+				$('#underlayTpDataTable').dataTable().fnAddData(rowData);
+			}
+		});
 
-
-function deleteController(objectId){
-	var requestUrl = "http://localhost:8080/org.openo.sdno.brs/openoapi/sdnobrs/v1/topological-links/"+objectId;
-		        $
-				.ajax({
-					type : "DELETE",
-					url : requestUrl,
-					contentType : "application/json",
-					success : function(jsonobj) {
-						$('#controller').bootstrapTable('remove', {
-		                    field: 'id',
-		                    values: [objectId]
-		                });
-						alert("Delete Controller successfull !!!");
-					},
-					error : function(xhr, ajaxOptions, thrownError) {
-						alert("Error on deleting controller : " + xhr.responseText); 	
-					}
-				});
+function deleteController(objectId) {
+	var requestUrl = "/openoapi/extsys/v1/sdncontrollers/" + objectId;
+	$.ajax({
+		type : "DELETE",
+		url : requestUrl,
+		contentType : "application/json",
+		success : function(jsonobj) {
+			$('#controller').bootstrapTable('remove', {
+				field : 'id',
+				values : [ objectId ]
+			});
+			alert("Delete Controller successfull !!!");
+		},
+		error : function(xhr, ajaxOptions, thrownError) {
+			alert("Error on deleting controller : " + xhr.responseText);
+		}
+	});
 }
 
-
-function loadControllerData(){
-	var requestUrl = "http://localhost:8080/org.openo.sdno.brs/openoapi/sdnobrs/v1/topological-links";
-		        $
-				.ajax({
-					type : "GET",
-					url : requestUrl,
-					contentType : "application/json",
-					success : function(jsonobj) {
-						$('#controller').bootstrapTable({
-			                data: jsonobj.topologicalControllers
-		                });
-					},
-					error : function(xhr, ajaxOptions, thrownError) {
-						alert("Error on getting controller data : " + xhr.responseText); 	
-					}
-				});
+function loadControllerData() {
+	var requestUrl = "/openoapi/extsys/v1/sdncontrollers";
+	$.ajax({
+		type : "GET",
+		url : requestUrl,
+		contentType : "application/json",
+		success : function(jsonobj) {
+			$('#controller').bootstrapTable({
+				data : jsonobj.topologicalControllers
+			});
+		},
+		error : function(xhr, ajaxOptions, thrownError) {
+			alert("Error on getting controller data : " + xhr.responseText);
+		}
+	});
 }
 
-$(function(){
-			$('.creat-btn').click(function(){
-				$('#vmAppDialog').addClass('in').css({'display':'block'});
-				
+$(function() {
+	$('.creat-btn').click(function() {
+		$('#vmAppDialog').addClass('in').css({
+			'display' : 'block'
+		});
+
+	});
+	$('.close,.button-previous').click(function() {
+		$('#vmAppDialog').removeClass('in').css('display', 'none');
+	});
+	$('.detail-top ul li').click(function() {
+		$(this).addClass('current').siblings().removeClass('current');
+	});
+	$('.para').click(function() {
+		if ($('#serviceTemplateName').val() == '') {
+			alert('Please choose the service templet！');
+			$('#flavorTab').css('display', 'none');
+		} else {
+			$('#flavorTab').css('display', 'block');
+		}
+		$('#basicTab').css('display', 'block');
+	});
+	$('.basic').click(function() {
+		$('#flavorTab').css('display', 'none');
+	});
+
+	$('.table tbody tr').click(
+			function() {
+				$(this).addClass('openoTable_row_selected').siblings()
+						.removeClass('openoTable_row_selected');
 			});
-			$('.close,.button-previous').click(function(){
-				$('#vmAppDialog').removeClass('in').css('display','none');
-			});
-			$('.detail-top ul li').click(function(){
-				$(this).addClass('current').siblings().removeClass('current');
-			});
-			$('.para').click(function(){				
-				if($('#serviceTemplateName').val() == ''){
-					alert('Please choose the service templet！');
-					$('#flavorTab').css('display','none');
-				}else{
-					$('#flavorTab').css('display','block');
-				}
-				$('#basicTab').css('display','block');
-			});
-			$('.basic').click(function(){
-				$('#flavorTab').css('display','none');
-			});
-			
-			$('.table tbody tr').click(function(){
-				$(this).addClass('openoTable_row_selected').siblings().removeClass('openoTable_row_selected');
-			});
-			$('.table tr:odd').addClass('active');
-			$('#false').click(function(){
-				$('#vmAppDialog').addClass('in').css({'display':'block'});
-			});
-			$('.close,.button-previous').click(function(){
-				$('#vmAppDialog').removeClass('in').css('display','none');
-			});
-			$('#filterTpLogicalType').click(function(){
-				$('#filterTpLogicalType_select_popupcontainer').toggleClass('openo-hide');
+	$('.table tr:odd').addClass('active');
+	$('#false').click(function() {
+		$('#vmAppDialog').addClass('in').css({
+			'display' : 'block'
+		});
+	});
+	$('.close,.button-previous').click(function() {
+		$('#vmAppDialog').removeClass('in').css('display', 'none');
+	});
+	$('#filterTpLogicalType').click(
+			function() {
+				$('#filterTpLogicalType_select_popupcontainer').toggleClass(
+						'openo-hide');
 				$('#filterTpLogicalType').toggleClass('openo-focus');
 				var oLeft = $('#open_base_tpL_td6').offset().left;
-			var oTop = $('#open_base_tpL_td6').offset().top;
-			var oHeight = $('#open_base_tpL_td6').height();
-			$('#filterTpLogicalType_select_popupcontainer').css({'left':oLeft,'top':oTop + oHeight + 10});
+				var oTop = $('#open_base_tpL_td6').offset().top;
+				var oHeight = $('#open_base_tpL_td6').height();
+				$('#filterTpLogicalType_select_popupcontainer').css({
+					'left' : oLeft,
+					'top' : oTop + oHeight + 10
+				});
 			});
-			$('div.openo-select-popup-container>div.openo-select-item>label').click(function(){
+	$('div.openo-select-popup-container>div.openo-select-item>label').click(
+			function() {
 				var Lvalue = $(this).html();
-				$('#filterTpLogicalType_select_input').attr('value',Lvalue);
-				$('#filterTpLogicalType_select_popupcontainer').addClass('openo-hide');
+				$('#filterTpLogicalType_select_input').attr('value', Lvalue);
+				$('#filterTpLogicalType_select_popupcontainer').addClass(
+						'openo-hide');
 				$('#filterTpLogicalType').removeClass('openo-focus');
 			});
-			$.fn.serializeObject = function() {
-				var o = {};
-				var a = this.serializeArray();
-				$.each(a, function() {
-					if (o[this.name] !== undefined) {
-						if (!o[this.name].push) {
-							o[this.name] = [ o[this.name] ];
-						}
-					o[this.name].push(this.value || '');
-					} else {
-						o[this.name] = this.value || '';
-					}
-				});
-			return o;
-			};
+	$.fn.serializeObject = function() {
+		var o = {};
+		var a = this.serializeArray();
+		$.each(a, function() {
+			if (o[this.name] !== undefined) {
+				if (!o[this.name].push) {
+					o[this.name] = [ o[this.name] ];
+				}
+				o[this.name].push(this.value || '');
+			} else {
+				o[this.name] = this.value || '';
+			}
+		});
+		return o;
+	};
 
-			$('#createController').click(function(){
-				var formData = JSON.stringify($("#controllerForm").serializeObject());
-				var jsonobj = JSON.parse(formData);
-		        var newJson = {"topologicalController": jsonobj};
-		        formData = JSON.stringify(newJson);
-		        var requestUrl = "http://localhost:8080/org.openo.sdno.brs/openoapi/sdnobrs/v1/topological-links";
-		        $
-				.ajax({
-					type : "POST",
-					url : requestUrl,
-					contentType : "application/json",
-					dataType : "json",
-					data : formData,
-					success : function(jsonResp) {
-						alert("Controller saved successfully!!!");
-						jsonobj["id"]= jsonResp.topologicalController.id;
-						$('#controller').bootstrapTable("append", jsonobj);
-						$('#vmAppDialog').removeClass('in').css('display','none');
-						
-					},
-					error : function(xhr, ajaxOptions, thrownError) {
-						alert("Error on page : " + xhr.responseText); 	
-					}
-				});
-			});
-			
-		})
+	$('#createController').click(function() {
+		var formData = JSON.stringify($("#controllerForm").serializeObject());
+		var jsonobj = JSON.parse(formData);
+		var newJson = {
+			"topologicalController" : jsonobj
+		};
+		formData = JSON.stringify(newJson);
+		var requestUrl = "/openoapi/extsys/v1/sdncontrollers";
+		$.ajax({
+			type : "POST",
+			url : requestUrl,
+			contentType : "application/json",
+			dataType : "json",
+			data : formData,
+			success : function(jsonResp) {
+				alert("Controller saved successfully!!!");
+				jsonobj["id"] = jsonResp.topologicalController.id;
+				$('#controller').bootstrapTable("append", jsonobj);
+				$('#vmAppDialog').removeClass('in').css('display', 'none');
+
+			},
+			error : function(xhr, ajaxOptions, thrownError) {
+				alert("Error on page : " + xhr.responseText);
+			}
+		});
+	});
+
+})
