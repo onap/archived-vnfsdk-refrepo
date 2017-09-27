36454ea0da07b7931b8859b22c8eff746237900a
Harry Huang <huangxiangyu5@huawei.com>
2017-09-27 18:23:13 +0800

    add UT cover for filemanage
    Change-Id: Ic67dd665c6641d48040bbbb8dec6b830a4d19f92
    Signed-off-by: Harry Huang <huangxiangyu5@huawei.com>
    Issue-Id: VNFSDK-85

---
+++ vnfmarket-be/vnf-sdk-marketplace/src/test/filemanage/FileManageTest.java
@@ -0,0 +1,107 @@
+/**
+ * Copyright 2017 Huawei Technologies Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.onap.vnfsdk.marketplace.filemanage;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.onap.vnfsdk.marketplace.filemanage.http.HttpFileManagerImpl;
+import org.onap.vnfsdk.marketplace.filemanage.http.ToolUtil;
+
+import mockit.Mock;
+import mockit.MockUp;
+
+public class FileManageTest {
+
+    @Test
+    public void testCreateFileManager() {
+        new MockUp<FileManagerFactory>() {
+            @Mock
+            private FileManagerType getType()  {
+                return FileManagerType.ftp;
+            }
+        };
+        FileManager manager = FileManagerFactory.createFileManager();
+
+        assertNull(manager);
+    }
+
+    @Before
+    public void createTestFile()
+    {
+        String srcPath = "./srcPathForTest";
+        String dstPath = "./dstPathForTest";
+        File src = new File(srcPath);
+        File dst = new File(dstPath);
+        src.mkdir();
+        dst.mkdir();
+    }
+
+    @Test
+    public void testDelete() throws IOException {
+        HttpFileManagerImpl ManagerImpl = new HttpFileManagerImpl();
+        String srcPath = "./srcPathForTest";
+        new MockUp<ToolUtil>() {
+            @Mock
+            private String getHttpServerAbsolutePath() {
+                return null;
+            }
+        };
+        new MockUp<ToolUtil>() {
+            @Mock
+            private boolean deleteDir(File dir) {
+                return true;
+            }
+        };
+
+        assertEquals(ManagerImpl.delete(srcPath), true);
+    }
+
+    @Test
+    public void testUpload() throws IOException {
+        HttpFileManagerImpl ManagerImpl = new HttpFileManagerImpl();
+        String srcPath = "./srcPathForTest";
+        String dstPath = "./dstPathForTest";
+        new MockUp<ToolUtil>() {
+            @Mock
+            private String getHttpServerAbsolutePath() {
+                return null;
+            }
+        };
+        new MockUp<ToolUtil>() {
+            @Mock
+            private boolean copyDirectory(String srcDirName, String destDirName, boolean overlay) {
+                return true;
+            }
+        };
+        assertEquals(ManagerImpl.upload(srcPath, dstPath), true);
+
+        File src = new File(srcPath);
+        if (src.exists())
+        {
+            src.delete();
+        }
+
+        assertEquals(ManagerImpl.upload(srcPath, dstPath), false);
+    }
+
+}
