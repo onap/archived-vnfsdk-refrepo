ad6c30a5bb8f9be36392d67a383b5ce274fd5d97
Avinash S <avinash.s@huawei.com>
2018-02-12 14:14:29 +0000

    Fixing nginx provisioning issues for https
    Issue-ID: VNFSDK-199
    
    Change-Id: I28bf3e5bfddd2b5b4efde131716f07e20111a81e
    Signed-off-by: Avinash S <avinash.s@huawei.com>

--- vnfmarket-be/deployment/docker/docker-refrepo/src/main/docker/certgen.sh
+++ vnfmarket-be/deployment/docker/docker-refrepo/src/main/docker/certgen.sh
@@ -1,2 +1,6 @@
-#/bin/sh
+#!/bin/sh
 openssl req -nodes -newkey rsa:2048 -keyout example.key -out example.csr -subj "/C=IN/ST=Bangalore/L=Bangalore/O=Global Security/OU=ONAP/CN=example.com"
+openssl x509 -req -in example.csr -signkey example.key -out cert.crt
+cp -p cert.crt /etc/nginx/ssl/
+systemctl enable nginx
+
