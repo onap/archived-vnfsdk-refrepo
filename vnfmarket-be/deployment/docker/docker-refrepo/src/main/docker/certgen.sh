d2802fbc8b27bd64abc36afd81f1fe27d2a4ea77
Avinash S <avinash.s@huawei.com>
2018-02-22 09:53:38 +0000

    Migrate container to ubuntu 16.04
    Change-Id: Ia4bc11843e3fb43722f50472213272abffe3c9b1
    Issue-ID: VNFSDK-211
    Signed-off-by: Avinash S <avinash.s@huawei.com>

--- vnfmarket-be/deployment/docker/docker-refrepo/src/main/docker/certgen.sh
+++ vnfmarket-be/deployment/docker/docker-refrepo/src/main/docker/certgen.sh
@@ -2,5 +2,5 @@
 openssl req -nodes -newkey rsa:2048 -keyout example.key -out example.csr -subj "/C=IN/ST=Bangalore/L=Bangalore/O=Global Security/OU=ONAP/CN=example.com"
 openssl x509 -req -in example.csr -signkey example.key -out cert.crt
 cp -p cert.crt /etc/nginx/ssl/
-systemctl enable nginx
-
+cp -p example.key /etc/nginx/ssl/cert.key
+#service nginx start & 
