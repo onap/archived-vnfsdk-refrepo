ba98dc9aa0f6e694f10065559acbe9f6aa83c887
Chris Donley <christopher.donley@huawei.com>
2018-03-26 16:24:40 -0700

    Remove security vulnerabilities
    Remove jackson.databind from FileUtil.java and LifecycleTestExceutor.java
    Remove unused test version of jquery 1.9.1.  Real jquery is 3.1.1.
    
    Issue-ID: VNFSDK-212
    
    Change-Id: Id8e0d7afa32a86cee371373ec6289f4e22ba2031
    Signed-off-by: Chris Donley <christopher.donley@huawei.com>

--- vnfmarket-be/vnf-sdk-marketplace/src/main/java/org/onap/vnfsdk/marketplace/onboarding/hooks/validatelifecycle/LifecycleTestExceutor.java
+++ vnfmarket-be/vnf-sdk-marketplace/src/main/java/org/onap/vnfsdk/marketplace/onboarding/hooks/validatelifecycle/LifecycleTestExceutor.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2017 Huawei Technologies Co., Ltd.
+ * Copyright 2017-2018 Huawei Technologies Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -33,10 +33,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
+/* commenting out to avoid security vulnerabilities
+ * import com.fasterxml.jackson.databind.DeserializationFeature;
+ * import com.fasterxml.jackson.databind.JsonMappingException;
+ * import com.fasterxml.jackson.databind.ObjectMapper;
+ */
 /* CALL Flow: onBoardingHandler --> LifecycleTestHook--> LifecycleTestExecutor */
 public class LifecycleTestExceutor {
 	private static final Logger logger = LoggerFactory.getLogger(LifecycleTestExceutor.class);
