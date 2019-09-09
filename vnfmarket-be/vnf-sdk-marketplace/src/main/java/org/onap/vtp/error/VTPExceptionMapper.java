2ead4645ee3ae49b1eb4030a6d654320447b34d5
priyanka.akhade <priyanka.akhade@huawei.com>
2019-09-09 12:58:13 +0000

    Replaced jackson with gson; Issue-ID: VNFSDK-369
    Signed-off-by: priyanka.akhade <priyanka.akhade@huawei.com>
    Change-Id: Ia289a5acdf687ffc1e9a7aceb6b8fb1010d29a52

--- vnfmarket-be/vnf-sdk-marketplace/src/main/java/org/onap/vtp/error/VTPExceptionMapper.java
+++ vnfmarket-be/vnf-sdk-marketplace/src/main/java/org/onap/vtp/error/VTPExceptionMapper.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2018 Huawei Technologies Co., Ltd.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,13 +21,12 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
+import com.google.gson.JsonObject;
 import org.apache.http.HttpStatus;
 import org.onap.vtp.error.VTPError.VTPException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.fasterxml.jackson.databind.node.JsonNodeFactory;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 
 @Provider
 @Singleton
@@ -35,17 +34,17 @@ public class VTPExceptionMapper implements ExceptionMapper<Exception> {
     private static final Logger LOG = LoggerFactory.getLogger(VTPExceptionMapper.class);
 
     public Response toResponse(Exception e) {
-         LOG.error(e.toString(), e);
+        LOG.error(e.toString(), e);
 
-         if (e instanceof VTPException) {
-             VTPException ex = (VTPException) e;
-             return Response.status(ex.getVTPError().getHttpStatus()).entity(ex.getVTPError().toString()).build();
-         } else {
-             ObjectNode node = JsonNodeFactory.instance.objectNode();
-             node.put("message", e.getMessage());
-             node.put("code", "UNKNOWN");
-             return Response.status(HttpStatus.SC_INTERNAL_SERVER_ERROR).entity(node.toString()).build();
-         }
+        if (e instanceof VTPException) {
+            VTPException ex = (VTPException) e;
+            return Response.status(ex.getVTPError().getHttpStatus()).entity(ex.getVTPError().toString()).build();
+        } else {
+            JsonObject node = new JsonObject();
+            node.addProperty("message", e.getMessage());
+            node.addProperty("code", "UNKNOWN");
+            return Response.status(HttpStatus.SC_INTERNAL_SERVER_ERROR).entity(node).build();
+        }
 
     }
 }
