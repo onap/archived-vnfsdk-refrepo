0d1ed8cf30201d3ad2fb5139357b2c812fdf6f8b
priyanka.akhade <priyanka.akhade@huawei.com>
2019-12-19 11:22:36 +0000

    Removed @JsonIgnoreProperties as gson ignores unknown properties by default; Issue-ID: VNFSDK-369
    Signed-off-by: priyanka.akhade <priyanka.akhade@huawei.com>
    Change-Id: Ifcd3e4bc605c8c54508d9ad941eb07751cb5546e

--- vnfmarket-be/vnf-sdk-marketplace/src/test/java/org/onap/vnfsdk/marketplace/rest/RestResponseTest.java
+++ vnfmarket-be/vnf-sdk-marketplace/src/test/java/org/onap/vnfsdk/marketplace/rest/RestResponseTest.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2017 Huawei Technologies Co., Ltd.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,6 +16,7 @@
 
 package org.onap.vnfsdk.marketplace.rest;
 
+import com.google.gson.Gson;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -25,10 +26,12 @@ import static org.junit.Assert.*;
 public class RestResponseTest {
 
     private RestResponse restResponse;
+    private Gson gson = null;
 
     @Before
     public void setUp() {
         restResponse = new RestResponse();
+        gson = new Gson();
     }
 
     @Test
@@ -36,6 +39,14 @@ public class RestResponseTest {
         restResponse.setResult("huawei");
         assertEquals(restResponse.getResult(), "huawei");
         restResponse.setStatusCode(200);
-        assertThat(restResponse.getStatusCode(),is(200));
+        assertThat(restResponse.getStatusCode(), is(200));
+    }
+
+    @Test
+    public void testRestResponseForUnknownFields() {
+        String jsonValue = "{\"statusCode\":\"200\",\"testField\":\"Unknown\"}";
+        RestResponse restResponse = gson.fromJson(jsonValue, RestResponse.class);
+        assertEquals(200, restResponse.getStatusCode().intValue());
+
     }
 }
