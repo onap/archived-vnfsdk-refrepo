abdc39a663bfad8cb4632d5cdd3e05ac1f47b46f
jitendra007 <jitendra.sharma1@huawei.com>
2020-08-04 12:13:53 +0530

    Fix license scan issues
    Issue-ID: VNFSDK-627
    
    Signed-off-by: jitendra007 <jitendra.sharma1@huawei.com>
    Change-Id: I760ba62cdfdd2023c70d9457ed87be4e5c95c3be

--- vnfmarket-be/deployment/docker/docker-refrepo/src/main/docker/certgen.sh
+++ vnfmarket-be/deployment/docker/docker-refrepo/src/main/docker/certgen.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
+#
+# Copyright 2020 Huawei Technologies Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 openssl req -nodes -newkey rsa:2048 -keyout example.key -out example.csr -subj "/C=IN/ST=Bangalore/L=Bangalore/O=Global Security/OU=ONAP/CN=example.com"
 openssl x509 -req -in example.csr -signkey example.key -out cert.crt
 cp -p cert.crt /etc/nginx/ssl/
