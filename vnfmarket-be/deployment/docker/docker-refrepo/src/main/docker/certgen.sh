65639ae1c34066f4cd82f378c31d06a4f6c9e27f
Michal Banka <michal.banka@nokia.com>
2020-10-02 14:35:30 +0200

    Update certs expiration date
    PROBLEM: By default certs were generated with 30 days of
    expiration days from image build date. When no new releases
    were made during that time certs were loosing validity and
    caused problems.
    
    SOLUTION: Updated generation process to set 730 days (2 years)
    until expiration date for new certs.
    Updated subject of new certs.
    
    Change-Id: I933cbbd1f80d824913cddbe19fc65988643d2282
    Signed-off-by: Michal Banka <michal.banka@nokia.com>
    Issue-ID: VNFSDK-650

--- vnfmarket-be/deployment/docker/docker-refrepo/src/main/docker/certgen.sh
+++ vnfmarket-be/deployment/docker/docker-refrepo/src/main/docker/certgen.sh
@@ -16,8 +16,8 @@
 # limitations under the License.
 #
 
-openssl req -nodes -newkey rsa:2048 -keyout example.key -out example.csr -subj "/C=IN/ST=Bangalore/L=Bangalore/O=Global Security/OU=ONAP/CN=example.com"
-openssl x509 -req -in example.csr -signkey example.key -out cert.crt
+openssl req -nodes -newkey rsa:2048 -keyout example.key -out example.csr -subj "/C=US/O=ONAP/OU=OSAAF/CN=intermediateCA_9/"
+openssl x509 -req -in example.csr -signkey example.key -days 730 -out cert.crt
 cp -p cert.crt /etc/nginx/ssl/
 cp -p example.key /etc/nginx/ssl/cert.key
 chmod 644 /etc/nginx/ssl/cert.crt
